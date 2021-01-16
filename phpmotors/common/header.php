2c59b520d87a239d6a21ec53be494e2c99186ddd
rudyravelin1 <rudyravelin@gmail.com>
2021-01-16 03:39:25 -0500

    Working on the css

--- phpmotors/common/header.php
+++ phpmotors/common/header.php
@@ -1,1 +1,2 @@
-
+<img src="/phpmotors/images/site/logo.png" alt="php-motors-logo" id="navLogo">
+<h1 id="navLink">My Account</h1>
---
+++ phpmotors/template.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Motors</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/phpmotors/css/style.css" media="screen">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Infant:wght@300&family=Playfair+Display&display=swap" rel="stylesheet">
+
+<body>
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/header.php'; ?>
+    </header>
+
+    <nav class="myMenu">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/navigation.php'; ?>
+    </nav>
+
+    <main>
+        <section>
+            <h1>Content Title Here</h1>
+        </section>
+    </main>
+
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
+    </footer>
+</body>
+
+</html>
