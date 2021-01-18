b9c0de1c5cbe3fbc09310ffe87ab3a122baa9e6f
rudyravelin1 <rudyravelin@gmail.com>
2021-01-18 15:04:53 -0500

    done with this assignment

--- phpmotors/home.php
+++ phpmotors/home.php
@@ -21,56 +21,58 @@
     </nav>
 
     <main>
-    <h2>Welcome to PHP Motors!</h2>
-        <section class="first-section">
-            <h2 id="title-1">DMC Delorean</h2>
-            <p id="paragraph-one">3 Cup holders<br>
-            Superman doors<br>
-            Fuzzy dice!</p>
+        <h2 class="heading-2">Welcome to PHP Motors!</h2>
 
-            <img class="delorean-car" src="../phpmotors/images/delorean.jpg" alt="Delorean car">
-            <img class="own-today" src="/phpmotors/images/site/own_today.png" alt="Today owner">
+        <section class="lastpart">
+            <div class="first-section">
+                <p id="title-1">DMC Delorean</p>
+                <p id="paragraph-one">3 Cup holders<br>
+                    Superman doors<br>
+                    Fuzzy dice!</p>
+            </div>
+            <div class="delocar">
+                <img class="delorean-car" src="../phpmotors/images/delorean.jpg" alt="Delorean car">
+                <img class="own-today" src="/phpmotors/images/site/own_today.png" alt="Today owner">
+            </div>
+
+            <h2 class="h2-reviews">DMC Delorean Reviews</h2>
+            <div class="reviews">
+                <ul id="li-reviews">
+                    <li>"So fast its almost like traveling in time." (4/5)</li>
+                    <li>"Coolest ride on the road." (4/5)</li>
+                    <li>"I'm feeling Marty McFly!." (5/5)</li>
+                    <li>"The most futuristic ride of our day." (4.5/5)</li>
+                    <li>"80's livin and I love it!." (5/5)</li>
+                </ul>
+            </div>
+            <h2 class="upgrade">Delorean Upgrades</h2>
+
+            <div class="container">
+                <div class="pic-1">
+                    <img src="/phpmotors/images/upgrades/flux-cap.png" alt="Flux Capacitor">
+                    <p><a href="#">Flux Capacitor</a></p>
+                </div>
+
+                <div class="pic-2">
+                    <img class="img-2" src="/phpmotors/images/upgrades/flame.jpg" alt="Flame Decals">
+                    <p><a href="#">Flame Decals</a></p>
+                </div>
+
+                <div class="pic-3">
+                    <img src="/phpmotors/images/upgrades/bumper_sticker.jpg" alt="Bumper Stickers">
+                    <p><a href="#">Bumper Stickers</a></p>
+                </div>
+
+                <div class="pic-4">
+                    <img class="img-4" src="/phpmotors/images/upgrades/hub-cap.jpg" alt="Hub Caps">
+                    <p class="hub-4"><a href="#">Hub Caps</a></p>
+                </div>
         </section>
 
-        <h2 class="h2-reviews">DMC Delorean Reviews</h2>
-        <div class="reviews">
-            <!-- <h2>DMC Delorean Reviews</h2> -->
-            <ul id="li-reviews">       
-                <li>"So fast its almost like traveling in time." (4/5)</li>
-                <li>"Coolest ride on the road." (4/5)</li>
-                <li>"I'm feeling Marty McFly!." (5/5)</li>
-                <li>"The most futuristic ride of our day." (4.5/5)</li>
-                <li>"80's livin and I love it!." (5/5)</li>
-            </ul>
-</div>
 
-<h2>Delorean Upgrades</h2>
-
-        <div class="container">
-        <div class="pic-1">
-            <img src="/phpmotors/images/upgrades/flux-cap.png" alt="Flux Capacitor">
-            <p><a href="#">Flux Capacitor</a></p>
-           </div>
-            
-            <div class="pic-2">
-            <img src="/phpmotors/images/upgrades/flame.jpg" alt="Flame Decals">
-            <p><a href="#">Flame Decals</a></p>
-            </div>
-
-            <div class="pic-3">
-            <img src="/phpmotors/images/upgrades/bumper_sticker.jpg" alt="Bumper Stickers">
-            <p><a href="#">Bumper Stickers</a></p>
-            </div>
-
-            <div class="pic-4">
-            <img src="/phpmotors/images/upgrades/hub-cap.jpg" alt="Hub Caps">
-            <p><a href="#">Hub Caps</a></p>
-            </div>
-
-            </div>
     </main>
 
-    <hr>  
+    <hr>
 
 
     <footer>
