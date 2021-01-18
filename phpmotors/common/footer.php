287c07a5afdc406784aec0c56daaaaebc87b2b1d
rudyravelin1 <rudyravelin@gmail.com>
2021-01-18 07:24:50 -0500

    Puttign all the HTML section in

--- phpmotors/common/footer.php
+++ phpmotors/common/footer.php
@@ -1,6 +1,6 @@
 <p>
     &copy; PHP Motors, All rights reserved.<br>
-    All images used are believed to be in " fair Use". Please Notify the authr if
+    All images used are believed to be in " Fair Use". Please Notify the authr if
     any are not and they will be removed.<br>
     <?php
     echo "Last updated: " . date("F d, Y.", getlastmod()) . "\n";
--- phpmotors/home.php
+++ phpmotors/home.php
@@ -1,4 +1,61 @@
-<?php
-echo "<h1>Hello</h1>";
+<!DOCTYPE html>
+<html lang="en">
 
-?>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Motors</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/phpmotors/css/home.css" media="screen">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Infant:wght@300&family=Playfair+Display&display=swap" rel="stylesheet">
+
+<body>
+    <header>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/header.php'; ?>
+    </header>
+
+    <nav class="myMenu">
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/navigation.php'; ?>
+    </nav>
+
+    <main>
+        <section>
+            <h2>Welcome to PHP Motors!</h2><br>
+            <h2>DMC Delorean</h3>
+            <p>3 Cup holders</p>
+            <p>Superman doors</p>
+            <p>Fuzzy dice!</p>
+            <img src="../phpmotors/images/delorean.jpg" alt="Delorean car">
+            <img src="/phpmotors/images/site/own_today.png" alt="Today owner">
+        </section>
+
+        <section>
+            <h2>DMC Delorean Reviews</h2>
+            <ol>
+                <li>."So fast its almost like traveling in time." (4/5)</li>
+                <li>."Coolest ride on the road." (4/5)</li>
+                <li>."I'm feeling Marty McFly!." (5/5)</li>
+                <li>."The most futuristic ride of our day." (4.5/5)</li>
+                <li>."80's livin and I love it!." (5/5)</li>
+            </ol>
+        </section>
+        <section>
+            <h2>Delorean Upgrades</h2>
+            <img src="/phpmotors/images/upgrades/flux-cap.png" alt="Flux Capacitor">
+            <img src="/phpmotors/images/upgrades/flame.jpg" alt="Flame Decals">
+            <img src="/phpmotors/images/upgrades/bumper_sticker.jpg" alt="Bumper Stickers">
+            <img src="/phpmotors/images/upgrades/hub-cap.jpg" alt="Hub Caps">
+        </section>
+    </main>
+
+    <hr>  
+
+
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/phpmotors/common/footer.php'; ?>
+    </footer>
+</body>
+
+</html>
